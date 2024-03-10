75147e44a0e493f9e88f727c9116171d5252095b
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-03-10 23:01:30 +0600

    country more information added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -3,11 +3,15 @@ import './Country.css'
 
 const Country = ({country}) => {
       // console.log(country);
-      const {name, flags} = country;
+      const {name, flags,population, area, ccn3} = country;
       return (
             <div className='country'>
                   <h5>Name : {name.common}</h5>
-                  <img src={flags.png} alt="flags" />
+                  <img src={flags.png} alt="flags"  style={{height:'300px'}}/>
+                  <p>Population : {population}</p>
+                  <p>Area : {area}</p>
+                  <p><small>Code : {ccn3}</small></p>
+                  <button>Visited</button>
             </div>
       );
 };
