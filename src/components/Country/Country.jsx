bc8e395fef52e38b6b009858b44c1e45f934f6cf
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-03-10 23:44:23 +0600

    country name display on list item

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountries}) => {
       // console.log(country);
       const {name, flags,population, area, ccn3} = country;
+
+      const [visited, setVisited] = useState(false)
+
+
+      const handleVisited = () => {
+            setVisited(!visited);
+      }
+      console.log(handleVisitedCountries);
       return (
-            <div className='country'>
-                  <h5>Name : {name.common}</h5>
-                  <img src={flags.png} alt="flags"  style={{height:'300px'}}/>
+            <div className={`country ${visited && 'visited'}`}>
+                  <h2 style={{color : visited ? 'purple': 'red'}}>Name : {name.common}</h2>
+                  <img src={flags.png} alt="flags" />
                   <p>Population : {population}</p>
                   <p>Area : {area}</p>
                   <p><small>Code : {ccn3}</small></p>
-                  <button>Visited</button>
+                  <button onClick={()=>handleVisitedCountries(country)}>Mark Visited</button>
+                  <button onClick={handleVisited}>{visited ? 'Visited': 'Going'}</button>
+                  {visited && "I have visited this country"}
             </div>
       );
 };
